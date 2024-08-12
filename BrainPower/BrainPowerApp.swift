8f42111f9c2ed03ddf21a8b8971ff24f2a981892
Yabby1997 <yabby1997@gmail.com>
2024-08-12 22:21:31 +0900

    Initial Commit

---
+++ BrainPower/BrainPowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainPowerApp.swift
+//  BrainPower
+//
+//  Created by user on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BrainPowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
