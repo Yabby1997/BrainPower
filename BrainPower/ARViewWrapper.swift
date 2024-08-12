cc8f0ce37f16776312d25d02855c0f7ac4efe158
Yabby1997 <yabby1997@gmail.com>
2024-08-13 01:31:34 +0900

    Implement body tracking skeleton

--- BrainPower/ARViewWrapper.swift
+++ BrainPower/ARViewWrapper.swift
@@ -5,16 +5,41 @@
 //  Created by user on 8/12/24.
 //
 
+import ARKit
 import RealityKit
 import SwiftUI
 
 struct ARViewWrapper: UIViewRepresentable {
-    func makeUIView(context: Context) -> some UIView {
+    func updateUIView(_ uiView: ARView, context: Context) {}
+
+    func makeUIView(context: Context) -> ARView {
         let view = ARView(frame: .zero)
-        let anchor = try! Experience.loadCube()
-        view.scene.addAnchor(anchor)
+        let bodyTrackingConfiguration = ARBodyTrackingConfiguration()
+        view.session.run(bodyTrackingConfiguration)
+        view.session.delegate = context.coordinator
+        view.scene.addAnchor(context.coordinator.bodyTrackingAnchor)
         return view
     }
 
-    func updateUIView(_ uiView: UIViewType, context: Context) {}
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+
+    class Coordinator: NSObject, ARSessionDelegate {
+        var skeleton: Skeleton?
+        let bodyTrackingAnchor: AnchorEntity = AnchorEntity()
+
+        public func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+            anchors.compactMap { $0 as? ARBodyAnchor }.forEach { [weak self] bodyAnchor in
+                guard let self else { return }
+                if let skeleton {
+                    skeleton.update(with: bodyAnchor)
+                } else {
+                    let skeleton = Skeleton(for: bodyAnchor)
+                    bodyTrackingAnchor.addChild(skeleton)
+                    self.skeleton = skeleton
+                }
+            }
+        }
+    }
 }
---
+++ BrainPower/Joint.swift
@@ -0,0 +1,13 @@
+//
+//  Joint.swift
+//  BrainPower
+//
+//  Created by user on 8/12/24.
+//
+
+import Foundation
+
+struct Joint {
+    let name: String
+    let position: SIMD3<Float>
+}
